aa3edf34a59108bd945e3094243447cfa6c1d4a1
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-09-26 03:18:29 +0600

    service animated added

--- components/home/Services.js
+++ components/home/Services.js
@@ -1,4 +1,5 @@
 import { services } from "@/data/services";
+import ShowOneByOne from "../animation/ShowOneByOne";
 
 export default function Services() {
   return (
@@ -6,23 +7,22 @@ export default function Services() {
       <h2 className="font-incognito text-text-root text-4xl font-semibold tracking-tight">
         My Services
       </h2>
-      <div className="py-10 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="py-10 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {services?.map((service, index) => (
-          <div
-            className="p-4  bg-[url('/gradient.svg')] bg-cover  overflow-hidden rounded-lg border border-project-card-border bg-project-card-bg-secondary  hover:scale-105 transition-all duration-500  backdrop-blur-3xl "
-            key={index}
-          >
-            <div>{service?.icon}</div>
-            <div>
-              <h3 className="text-text-primary text-2xl font-semibold py-2">
-                {service?.name}
-              </h3>
-              <p className="text-text-secondary">{service?.description}</p>
+          <ShowOneByOne index={index} key={index}>
+            <div className="p-4  bg-[url('/gradient.svg')] bg-cover  overflow-hidden rounded-lg border border-project-card-border bg-project-card-bg-secondary  hover:scale-105 transition-all duration-500  backdrop-blur-3xl ">
+              <div>{service?.icon}</div>
+              <div>
+                <h3 className="text-text-primary text-2xl font-semibold py-2">
+                  {service?.name}
+                </h3>
+                <p className="text-text-secondary">{service?.description}</p>
+              </div>
+              <div className="absolute top-0 -translate-y-1/3 mx-auto  right-0 w-3/4">
+                <div className="bg-[rgba(0,191,255,0.72)]  dark:bg-[rgba(255,0,174,0.75)] aspect-square  opacity-20 w-full max-w-full  rounded-full filter blur-3xl" />
+              </div>
             </div>
-            <div className="absolute top-0 -translate-y-1/3 mx-auto  right-0 w-3/4">
-              <div className="bg-[rgba(0,191,255,0.72)]  dark:bg-[rgba(255,0,174,0.75)] aspect-square  opacity-20 w-full max-w-full  rounded-full filter blur-3xl" />
-            </div>
-          </div>
+          </ShowOneByOne>
         ))}
       </div>
     </section>
--- data/services.js
+++ data/services.js
@@ -12,36 +12,84 @@ export const services = [
     name: "Front-End",
     description:
       "Responsive, scalable, and smooth user experiences, along with dynamic SPAs using React with Next.JS and Tailwind CSS.",
-    icon: <RiNextjsFill className="text-[65px] text-button " />,
+    icon: (
+      <RiNextjsFill
+        className=" text-[4em] text-button "
+        style={{
+          width: "4em",
+          height: "4em",
+        }}
+      />
+    ),
   },
   {
     name: "Backend-End",
     description:
       "Robust, scalable, high-performance, and easily maintainable RESTful and GraphQL APIs using Node.js and Express.",
-    icon: <IoLogoNodejs className="text-[65px] text-button " />,
+    icon: (
+      <IoLogoNodejs
+        className="text-[65px] text-button "
+        style={{
+          width: "4em",
+          height: "4em",
+        }}
+      />
+    ),
   },
   {
     name: "Hosting",
     description:
       "Setup, configuration, and management of hosting environments using platforms like DigitalOcean, Vercel, and cPanel.",
-    icon: <TfiServer className="text-[65px] text-button " />,
+    icon: (
+      <TfiServer
+        className="text-[65px] text-button "
+        style={{
+          width: "4em",
+          height: "4em",
+        }}
+      />
+    ),
   },
   {
     name: "Database Managment",
     description:
       "I ensure data integrity, security, and efficient access with SQL (MySQL, PostgreSQL) and NoSQL (MongoDB) databases.",
-    icon: <FaServer className="text-[65px] text-button " />,
+    icon: (
+      <FaServer
+        className="text-[65px] text-button "
+        style={{
+          width: "4em",
+          height: "4em",
+        }}
+      />
+    ),
   },
   {
     name: "Firebase",
     description:
       "Real-time databases, authentication, and hosting ensure secure, scalable and efficient performance with Firebase.",
-    icon: <IoLogoFirebase className="text-[65px] text-button " />,
+    icon: (
+      <IoLogoFirebase
+        className="text-[65px] text-button "
+        style={{
+          width: "4em",
+          height: "4em",
+        }}
+      />
+    ),
   },
   {
     name: "SEO",
     description:
       "Offer comprehensive SEO services,optimization, and ongoing analysis to improve visibility and rankings.",
-    icon: <IoSearchSharp className="text-[65px] text-button " />,
+    icon: (
+      <IoSearchSharp
+        className="text-[65px] text-button "
+        style={{
+          width: "4em",
+          height: "4em",
+        }}
+      />
+    ),
   },
 ];
