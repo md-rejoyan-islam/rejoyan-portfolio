52e6e01b0f5e017fe59ea82e2c0c34a02f0c7eb6
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-10-28 03:24:27 +0600

    update

---
+++ components/home/ServiceCard.jsx
@@ -0,0 +1,61 @@
+"use client";
+import { useAnimation } from "framer-motion";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function ServiceCard({ index, key, service }) {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const controls = useAnimation();
+
+  const handleMouseMove = (event, index) => {
+    const liElement = event.currentTarget;
+    const rect = liElement.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    setPosition({ x, y });
+    controls.start({ x, y });
+  };
+
+  return (
+    <div
+      className={`group relative z-0  h-full      overflow-hidden rounded-xl border border-project-card-border    transition-all duration-500 backdrop-blur-3xl `}
+      style={{
+        background: position.x
+          ? `radial-gradient(400px circle at ${position.x}px ${position.y}px, #ee6eff, transparent, transparent 70%)`
+          : "",
+      }}
+      onMouseMove={(event) => handleMouseMove(event, index)}
+    >
+      <div className="relative z-10 bg-project-card-bg-secondar p-5 h-full overflow-hidden  ">
+        <div>{service?.icon}</div>
+        <div>
+          <h3 className="text-text-primary text-2xl font-semibold py-2">
+            {service?.name}
+          </h3>
+          <p className="text-text-secondary">{service?.description}</p>
+        </div>
+        <div>
+          <Image
+            alt="Grid"
+            loading="lazy"
+            width={2880}
+            height={1358}
+            decoding="async"
+            data-nimg={1}
+            className="absolute inset-0 -z-10 h-full w-full object-cover  opacity-0 hover:opacity-100  group-hover:opacity-100 duration-150"
+            src="/bg-pattern.webp"
+          />
+        </div>
+      </div>
+      <div
+        className="absolute  h-4/5 w-4/5 bg-[linear-gradient(180deg,_#ee6eff,_rgba(59,_130,_246,_0.00)_137.53%,_rgba(32,_69,_129,_0.00)_195%)] dark:bg-[linear-gradient(180deg,_#ee6eff30,_rgba(59,_130,_246,_0.00)_137.53%,_rgba(32,_69,_129,_0.00)_195%)]  opacity-0 blur-[70px] duration-150 group-hover:opacity-90"
+        style={{
+          transform: "translate(-50%, -50%)",
+          top: `${position.y}px`,
+          left: `${position.x}px`,
+        }}
+      />
+      <div className="absolute inset-[1px] -z-10 rounded-xl bg-project-card-bg-secondary " />
+    </div>
+  );
+}
--- components/home/Services.js
+++ components/home/Services.js
@@ -1,5 +1,6 @@
 import { services } from "@/data/services";
 import ShowOneByOne from "../animation/ShowOneByOne";
+import ServiceCard from "./ServiceCard";
 
 export default function Services() {
   return (
@@ -7,21 +8,21 @@ export default function Services() {
       <h2 className="font-incognito text-text-root text-4xl font-semibold tracking-tight">
         My Services
       </h2>
-      <div className="py-10 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="py-10 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
         {services?.map((service, index) => (
           <ShowOneByOne index={index} key={index}>
-            <div className="p-4 h-full  bg-[url('/gradient.svg')] bg-cover  overflow-hidden rounded-lg border border-project-card-border bg-project-card-bg-secondary  hover:scale-105 transition-all duration-500  backdrop-blur-3xl ">
-              <div>{service?.icon}</div>
+            <ServiceCard service={service} key={index} index={index} />
+            {/* <div className="p-4 h-full   bg-cover  overflow-hidden rounded-lg border border-project-card-border bg-project-card-bg-secondary  transition-all duration-500  backdrop-blur-3xl ">
               <div>
-                <h3 className="text-text-primary text-2xl font-semibold py-2">
-                  {service?.name}
-                </h3>
-                <p className="text-text-secondary">{service?.description}</p>
+                <div>{service?.icon}</div>
+                <div>
+                  <h3 className="text-text-primary text-2xl font-semibold py-2">
+                    {service?.name}
+                  </h3>
+                  <p className="text-text-secondary">{service?.description}</p>
+                </div>
               </div>
-              <div className="absolute top-0 -translate-y-1/3 mx-auto  right-0 w-3/4">
-                <div className="bg-[rgba(0,191,255,0.72)]  dark:bg-[rgba(255,0,174,0.75)] aspect-square  opacity-20 w-full max-w-full  rounded-full filter blur-3xl" />
-              </div>
-            </div>
+            </div> */}
           </ShowOneByOne>
         ))}
       </div>
