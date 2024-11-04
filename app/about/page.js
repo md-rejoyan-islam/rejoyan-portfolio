0ce609d3c0f7893c921343fc8d4f454cf0d3e9c9
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-11-04 10:39:12 +0600

    update

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,17 @@
+import Image from "next/image";
+import photo from "./image.png";
+
 export default function Experience() {
   return (
     <div>
       <h2 className="text-text-primary py-10 text-center">Comming Soon</h2>
+      <Image
+        src={photo}
+        alt="Coming Soon"
+        width={500}
+        height={500}
+        className="mx-auto"
+      />
     </div>
   );
 }
